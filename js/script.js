17fd70c679cc9e3282f4320c42d957c2bcdcae20
Erison <erisonn.santos@gmail.com>
2021-03-29 10:34:38 -0300

    add numbers

---
+++ js/script.js
@@ -0,0 +1,56 @@
+
+const limpar = document.getElementById('limpar');
+limpar.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value = ''
+})
+
+// numeros
+const n1 = document.getElementById('n1');
+n1.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 1
+})
+
+const n2 = document.getElementById('n2');
+n2.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 2
+})
+
+const n3 = document.getElementById('n3');
+n3.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 3
+})
+
+const n4 = document.getElementById('n4');
+n4.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 4
+})
+
+const n5 = document.getElementById('n5');
+n5.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 5
+})
+
+const n6 = document.getElementById('n6');
+n6.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 6
+})
+
+const n7 = document.getElementById('n7');
+n7.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 7
+})
+
+const n8 = document.getElementById('n8');
+n8.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 8
+})
+
+const n9 = document.getElementById('n9');
+n9.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 9
+})
+
+const n0 = document.getElementById('n0');
+n0.addEventListener('click', function() {
+    document.getElementsByClassName('resultado')[0].value += 0
+})
